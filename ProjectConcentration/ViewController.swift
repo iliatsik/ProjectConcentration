aaa211f1226bd45189032ffdc78a959eb6894033
iliatsik <ilia.tsikelashvili@quantori.com>
2022-01-05 11:23:17 +0400

    - Initial commit

---
+++ ProjectConcentration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectConcentration
+//
+//  Created by Ilia Tsikelashvili on 05.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
